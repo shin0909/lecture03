75cdf07c10528fc8529269c595ce229a3d79b3e5
shin0909 <s13845sm@sfc.keio.ac.jp>
2014-04-24 12:36:24 +0900

    新しいボタンを追加

--- js/helloworld2.js
+++ js/helloworld2.js
@@ -1,13 +1,4 @@
 var helloWorld = function(){
-	var message = document.getElementById("helloWorld");
-	message.setAttribute("class", "");
-};
-
-var showHelloWorldButton = document.getElementById("showHelloWorld");
-showHelloWorldButton.addEventListener("click", helloWorld);
-
-
-var helloWorld2 = function(){
 	var message2 = document.getElementById("helloWorld2");
 	message2.setAttribute("class", "");
 };
